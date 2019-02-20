e5537fafe38e7b797db1cec39813a18036e81854
unknown <noha.hussan@gmail.com>
2019-02-19 22:54:05 -0500

    submit lab 23

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Sessions.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
+        public User(string Name, string Email, string Password)
+        {
+            this.Name = Name;
+            this.Email = Email;
+            this.Password = Password;
+        }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
